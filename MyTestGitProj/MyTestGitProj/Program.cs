0b03ac2f4a38fe1a0fea88d57e6c9ea4d7ef9457
DermentzisGeorgios <george.gd6@gmail.com>
2020-07-28 20:19:27 +0300

    My third commit

--- MyTestGitProj/MyTestGitProj/Program.cs
+++ MyTestGitProj/MyTestGitProj/Program.cs
@@ -11,7 +11,8 @@ namespace MyTestGitProj
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-
+            string x = Console.ReadLine();
+            Console.WriteLine(x);
         }
     }
 }
