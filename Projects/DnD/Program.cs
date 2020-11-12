24786d1895dab14476d83b2395c6328068b8e541
DermentzisGeorgios <george.gd6@gmail.com>
2020-11-13 00:36:16 +0200

    Added my first 2 projects

---
+++ Projects/DnD/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DnD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> attributes = new List<string>
+            {
+                "strength",
+                "dexterity",
+                "constitution",
+                "intelligence",
+                "wisdom",
+                "charisma"
+            };
+            List<int> stats = CreateStats();
+            int hitpoints = Calchitpoints(stats[2], out double dmg);
+            string charClass = SelectClass();
+            stats = ChangeAttributes(stats, charClass);
+            double xphas =0;
+            double xpgain = Xp_gain();
+            int lvl = Level(xphas, xpgain);
+            Console.WriteLine("Class: {0}\nLvl: {1}\nHP: {2}", charClass, lvl, hitpoints);
+            for (int i = 0; i < 6; i++)
+                Console.WriteLine("{0}: {1}", attributes[i], stats[i]);
+        }
+
+        public static List<int> CreateStats()
+        {
+            List<int> stats = new List<int>();
+            Random rand = new Random();
+            int sum;
+            int i = 0;
+            while (i < 6)
+            {
+                sum = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    sum += rand.Next(1, 7);
+                }
+                if (sum >= 14 && sum <= 20)
+                {
+                    stats.Add(sum);
+                    i++;
+                }
+            }
+            return stats;
+        }
+
+        public static int Calchitpoints(int cons, out double dmg)
+        {
+            double hitpoints = (10 - cons) / 2.0;
+            hitpoints += 0.01;
+            hitpoints = 10 + Math.Round(hitpoints);
+            dmg = 4 * hitpoints;
+            return Convert.ToInt32(hitpoints);
+        }
+
+        public static string SelectClass()
+        {
+            Console.WriteLine("Please select your class");
+            string[] classes = new string[12] { "Barbarian", "Fighter",
+                                                "Bard", "Sorcerer", "Paladin", "Warlock",
+                                                "Cleric", "Druid", "Monk",
+                                                "Rogue", "Ranger",
+                                                "Wizard" };
+            int intClass;
+            bool flag = false;
+            do
+            {
+                for (int i = 1; i <= 12; i++)
+                {
+                    Console.WriteLine("{0}. {1}", i, classes[i - 1]);
+                }
+                if (int.TryParse(Console.ReadLine(), out intClass))
+                    flag = true;
+            } while (!flag || intClass < 1 || intClass > 12);
+            return classes[intClass - 1];
+        }
+
+        public static List<int> ChangeAttributes(List<int> stats, string charClass)
+        {
+            switch (charClass)
+            {
+                case "Barbarian":
+                case "Fighter":
+                    stats = ChangeStats(stats, 0);
+                    break;
+                case "Bard":
+                case "Sorcerer":
+                case "Warlock":
+                    stats = ChangeStats(stats, 5);
+                    break;
+                case "Cleric":
+                case "Druid":
+                case "Monk":
+                    ChangeStats(stats, 4);
+                    break;
+                case "Rogue":
+                case "Ranger":
+                    stats = ChangeStats(stats, 1);
+                    break;
+                case "Wizard":
+                    stats = ChangeStats(stats, 3);
+                    break;
+                case "Paladin":
+                    stats = PaladinStats(stats);
+                    break;
+            }
+            return stats;
+        }
+
+        public static List<int> ChangeStats(List<int> stats, int p)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (i == p)
+                    stats[i] = stats[i] * 2;
+                else
+                {
+                    double tmp = stats[i] * 0.8;
+                    tmp += 0.01;
+                    stats[i] = Convert.ToInt32(Math.Round(tmp));
+                }
+            }
+            return stats;
+        }
+
+        public static List<int> PaladinStats(List<int> stats)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (i == 0 || i == 5)
+                    stats[i] = stats[i] * 2;
+                else
+                {
+                    double tmp = stats[i] * 0.8;
+                    tmp += 0.01;
+                    stats[i] = Convert.ToInt32(Math.Round(tmp));
+                }
+            }
+            return stats;
+        }
+
+        public static int Level(double xphas, double xpgain)
+        {
+            int lvl = 0;
+            int diff = 0;
+            if (xpgain >= 100)
+            {
+                do
+                {
+                    lvl++;
+                    diff += 100;
+                    xphas += diff;
+                } while (xpgain >= xphas);
+            }
+            else
+                lvl = 1;
+            return lvl;
+        }
+
+        public static double Xp_gain()
+        {
+            double xpgain;
+            do
+            {
+                Console.WriteLine("How many xp have you gained?");
+            } while (!double.TryParse(Console.ReadLine(), out xpgain) || xpgain < 0);
+            return xpgain;
+        }
+    }
+}
---
+++ Projects/Project1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output;
+            string FavColor;
+            Console.WriteLine("What is your name?");
+            string Name = Console.ReadLine();
+            Console.WriteLine("What year you where born?");
+            int BirthYear;
+            while (int.TryParse(Console.ReadLine(), out BirthYear) == false || BirthYear < 1900)
+            {
+                Console.WriteLine("That's not a valid year. Please try again.");
+                Console.WriteLine("What year you where born?");
+            }
+            int Age = DateTime.Now.Year - BirthYear;
+            Console.WriteLine("What is the name of the city you where born?");
+            string BirthCity = Console.ReadLine();
+            Console.WriteLine("Are you married?");
+            string married = Console.ReadLine().ToLower();
+            if (married == "yes")
+            {
+                Console.WriteLine("How many years have you been married?");
+                int YearsMarried;
+                while (int.TryParse(Console.ReadLine(), out YearsMarried) == false || YearsMarried < 0 || YearsMarried >= Age)
+                {
+                    Console.WriteLine("That's not a valid year. Please try again.");
+                    Console.WriteLine("How many years have you been married?");
+                }
+                int YearOfMarriage = DateTime.Now.Year - YearsMarried;
+                Console.WriteLine("Do you have any children?");
+                string children = Console.ReadLine().ToLower();
+                if (children == "yes")
+                {
+                    Console.WriteLine("How many children do you have?");
+                    int ChildrenNumber;
+                    while (int.TryParse(Console.ReadLine(), out ChildrenNumber) == false || ChildrenNumber <= 0)
+                    {
+                        Console.WriteLine("That's not a valid number. Please try again.");
+                        Console.WriteLine("How many children do you have?");
+                    }
+                    string[] ChildName = new string[ChildrenNumber];
+                    int[] ChildAge = new int[ChildrenNumber];
+                    output = "Dear " + Name + ", you were born in " + BirthYear + " so you are " + Age + " and born in the city of " + BirthCity + ".\nYou have been married for " + YearsMarried + " and you got married in " + YearOfMarriage + " and you have " + ChildrenNumber + " children.\n";
+                    for (int i = 0; i < ChildrenNumber; i++)
+                    {
+                        Console.WriteLine("What is the name of your {0} child?", i + 1);
+                        ChildName[i] = Console.ReadLine();
+                        Console.WriteLine("What is the age of your {0} child?", i + 1);
+                        while (int.TryParse(Console.ReadLine(), out ChildAge[i]) == false || ChildAge[i] < 0 || ChildAge[i] >= Age)
+                        {
+                            Console.WriteLine("That's not a valid age. Please try again.");
+                            Console.WriteLine("What is the age of your {0} child?", i + 1);
+                        }
+                        output = output + "The name of your " + (i+1) + " child is " + ChildName[i] + " and was born in the year of " + ChildAge[i] + ".\n";
+                    }
+                    output = output + "Your favourite color is ";
+                }
+                else
+                    output = "Dear " + Name + ", you were born in " + BirthYear + " so you are " + Age + " and born in the city of " + BirthCity + ".\nYou have been married for " + YearsMarried + " and you got married in " + YearOfMarriage + " and you don't have any children.\nYour favourite color is ";
+            }
+            else
+                output = "Dear " + Name + ", you were born in " + BirthYear + " so you are " + Age + " and born in the city of " + BirthCity + ".\nYou are not married and your favourite color is ";
+            Console.WriteLine("What is your favourite color?");
+            FavColor = Console.ReadLine();
+            Console.WriteLine(output + FavColor + ".");
+        }
+    }
+}
