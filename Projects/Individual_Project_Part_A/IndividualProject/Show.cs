3ad3a5b38649d03ad5ead215510788a826ff7ade
DermentzisGeorgios <george.gd6@gmail.com>
2021-02-07 06:06:39 +0200

    Rename

---
+++ Projects/Individual_Project_Part_A/IndividualProject/Show.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndividualProject
+{
+    static class Show
+    {
+        public static void Lists(List<Courses> courseList, List<Students> studentList, List<Trainers> trainerList, List<Assignments> assignmentList, List<StudentsPerCourse> studentsPerCourse, List<TrainersPerCourse> trainersPerCourse, List<AssignmentsPerCourse> assignmentsPerCourse, List<AssignmentsPerStudent> assignmentsPerStudent, List<Students> someStudentList, List<DateTime> dateWeek, List<CoursesPerStudent> coursesPerStudent, int x)
+        {
+            try
+            {
+                switch (x)
+                {
+                    case 1:
+                        foreach (Courses obj in courseList)
+                        {
+                            obj.ShowCourses();
+                        }
+                        break;
+                    case 2:
+                        foreach (Students obj in studentList)
+                        {
+                            obj.ShowStudents();
+                        }
+                        break;
+                    case 3:
+                        foreach (Trainers obj in trainerList)
+                        {
+                            obj.ShowTrainers();
+                        }
+                        break;
+                    case 4:
+                        foreach (Assignments obj in assignmentList)
+                        {
+                            obj.ShowAssignments();
+                        }
+                        break;
+                    case 5:
+                        foreach (StudentsPerCourse obj in studentsPerCourse)
+                        {
+                            obj.ShowStudentsPerCourse();
+                        }
+                        break;
+                    case 6:
+                        foreach (TrainersPerCourse obj in trainersPerCourse)
+                        {
+                            obj.ShowTrainersPerCourse();
+                        }
+                        break;
+                    case 7:
+                        foreach (AssignmentsPerCourse obj in assignmentsPerCourse)
+                        {
+                            obj.ShowAssignmentsPerCourse();
+                        }
+                        break;
+                    case 8:
+                        foreach (AssignmentsPerStudent obj in assignmentsPerStudent)
+                        {
+                            obj.ShowAssignmentsPerStudentPerCourse(coursesPerStudent);
+                        }
+                        break;
+                    case 9:
+                        foreach (Students obj in someStudentList)
+                        {
+                            obj.ShowStudents();
+                        }
+                        break;
+                    case 10:
+                        AssignmentsPerStudent temp = new AssignmentsPerStudent();
+                        temp.ShowStudentAssignmentWeek(dateWeek, assignmentsPerStudent);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Projects/Individual_Project_Part_A/IndividualProject/Trainers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndividualProject
+{
+    class Trainers
+    {
+        string firstName { get;}
+        string lastName { get; }
+        string subject { get;}
+        readonly int ID;
+
+        public Trainers(string firstName, string lastName, string subject, int ID)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.subject = subject;
+            this.ID = ID;
+        }
+
+        public void ShowTrainers()
+        {
+            Console.WriteLine($"{ID}. {lastName} {firstName}, works on {subject}.");
+        }
+
+        public bool NotEquals(Trainers trainer1, Trainers trainer2)
+        {
+            bool notEquals;
+            if (trainer1.firstName != trainer2.firstName || trainer1.lastName != trainer2.lastName || trainer1.subject != trainer2.subject)
+                notEquals = true;
+            else
+                notEquals = false;
+            return notEquals;
+        }
+    }
+
+    class TrainersPerCourse
+    {
+        Courses _course { get;}
+        List<Trainers> _trainersPerCourseList { get;}
+
+        public TrainersPerCourse(Courses course, List<Trainers> trainersPerCourseList)
+        {
+            _course = course;
+            _trainersPerCourseList = trainersPerCourseList;
+        }
+
+        public void ShowTrainersPerCourse()
+        {
+            try
+            {
+                Console.WriteLine("\nCourse:");
+                _course.ShowCourses();
+                Console.WriteLine("\nTrainers:");
+                foreach (Trainers obj in _trainersPerCourseList)
+                {
+                    obj.ShowTrainers();
+                }
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
